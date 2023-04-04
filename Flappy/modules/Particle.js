1338006351c2d594d6b1eaef04e52b14d6cbdae6
Denis <greenfish@list.ru>
2023-04-04 08:47:40 +0400

    add particles

--- Flappy/modules/Particle.js
+++ Flappy/modules/Particle.js
@@ -1,6 +1,24 @@
 class Particle {
-  constructor(player) {
+  constructor(player, gamespeed, ctx) {
     this.x = player.x;
+    this.y = player.y + player.height;
+    this.color = 'gray';
+    this.vy = Math.random() * 1 - 0.5;
+    this.vx = gamespeed;
+    this.ctx = ctx;
+    this.radius = Math.random() * 7 + 3;
+  }
+
+  update() {
+    this.x -= this.vx;
+    this.y += this.vy;
+  }
+
+  draw() {
+    this.ctx.fillStyle = this.color;
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    this.ctx.fill();
   }
 }
 
--- Flappy/script.js
+++ Flappy/script.js
@@ -6,11 +6,29 @@ let canvas = document.getElementById('canvas1');
 let ctx = canvas.getContext('2d');
 let score = 0;
 let gamespeed = 2;
+const particles = [];
+const particlesQuantity = 20;
 
 canvas.width = 500;
 canvas.height = 500;
 
 const player = new Character(canvas, ctx);
+
+const pushParticles = () => {
+  particles.unshift(new Particle(player, gamespeed, ctx));
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].draw();
+  }
+  //TODO check if particles outside the canvas
+  if (particles.length > 200) {
+    for (let i = 0; i < 20; i++) {
+      particles.pop();
+    }
+  }
+};
+
+const removeParticles = () => {};
 
 window.addEventListener('keydown', (e) => {
   if (e.code === 'Space') player.isMove = true;
@@ -26,7 +44,7 @@ function animate() {
   player.update();
   player.draw();
 
+  pushParticles();
   requestAnimationFrame(animate);
 }
-
 animate();
