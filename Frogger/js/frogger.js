6527a9c21d8cac6e4bf14a9d4be6d1b64dff342a
Denis Sazonov <greenfish@list.ru>
2023-04-18 08:46:02 +0400

    morning 18 april

---
+++ Frogger/js/frogger.js
@@ -0,0 +1,8 @@
+class Frogger {
+  constructor() {
+    this.spriteWidth = 250;
+    this.spriteHeight = 250;
+    this.width = this.spriteWidth / 5;
+    this.height = this.spriteHeight / 5;
+  }
+}
