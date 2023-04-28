6441d871bdd12f3a1cd00fe6f4d85e49f17af167
Denis Sazonov <greenfish@list.ru>
2023-04-28 08:32:13 +0400

    make ripples effect

--- Frogger/js/particles.js
+++ Frogger/js/particles.js
@@ -8,8 +8,8 @@ class Particle {
     this.directionY = Math.random() * 2 - 0.5;
   }
   draw() {
-    this.opacity -= 0.03;
-    if (this.opacity < 0) this.opacity = 0;
+    if (this.opacity > 0.1) this.opacity -= 0.9;
+    if (this.radius > 0.15) this.radius -= 0.14;
     ctx3.fillStyle = 'rgba(150, 150, 150, ' + this.opacity + ')';
     ctx3.beginPath();
     ctx3.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
@@ -21,9 +21,12 @@ class Particle {
     this.y += this.directionY;
   }
   drawRipples() {
-    ctx3.strokeStyle = 'white';
+    if (this.opacity > 0.05) {
+      this.opacity -= 0.05;
+    }
+    ctx3.strokeStyle = 'rgba(255, 255, 255, ' + this.opacity + ')';
     ctx3.beginPath();
-    if (this.radius < 30) {
+    if (this.radius < 50) {
       this.radius += 0.5;
     }
     ctx3.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
@@ -33,17 +36,18 @@ class Particle {
 }
 
 function handleParticles() {
+  //dust
   for (let particle of particlesArray) {
-    if (frogger.y > 200) {
-      particle.update();
-      particle.draw();
-    }
+    particle.update();
+    particle.draw();
   }
+
   if (particlesArray.length > maxParticles) {
     for (let i = 0; i < 30; i++) {
       particlesArray.pop();
     }
   }
+
   if (
     (keys['KeyA'] ||
       keys['KeyW'] ||
@@ -53,11 +57,39 @@ function handleParticles() {
       keys['ArrowUp'] ||
       keys['ArrowRight'] ||
       keys['ArrowDown']) &&
-    frogger.y > 200 &&
+    frogger.y > 250 &&
     particlesArray.length < maxParticles + 10
   ) {
     for (let i = 0; i < 10; i++) {
       particlesArray.unshift(new Particle(frogger.x, frogger.y));
+    }
+  }
+
+  //water
+  for (let ripple of ripplesArray) {
+    ripple.drawRipples();
+  }
+
+  if (ripplesArray.length > 20) {
+    for (let i = 0; i < 10; i++) {
+      ripplesArray.pop();
+    }
+  }
+
+  if (
+    (keys['KeyA'] ||
+      keys['KeyW'] ||
+      keys['KeyD'] ||
+      keys['KeyS'] ||
+      keys['ArrowLeft'] ||
+      keys['ArrowUp'] ||
+      keys['ArrowRight'] ||
+      keys['ArrowDown']) &&
+    frogger.y > 100 &&
+    frogger.y < 250
+  ) {
+    for (let i = 0; i < 10; i++) {
+      ripplesArray.unshift(new Particle(frogger.x, frogger.y));
     }
   }
 }
